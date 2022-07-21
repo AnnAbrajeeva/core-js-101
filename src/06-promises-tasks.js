ff8422cf62e04f2d67331c759e9f420b05a60bd3
Ann Abrajeeva <anuta040786@yandex.ru>
2022-07-21 18:39:24 +0500

    feat: done promises-task

--- src/06-promises-tasks.js
+++ src/06-promises-tasks.js
@@ -5,7 +5,6 @@
  *                                                                                                *
  ************************************************************************************************ */
 
-
 /**
  * Return Promise object that is resolved with string value === 'Hooray!!! She said "Yes"!',
  * if boolean value === true is passed, resolved with string value === 'Oh no, she said "No".',
@@ -28,10 +27,21 @@
  *      .catch((error) => console.log(error.message)) // 'Error: Wrong parameter is passed!
  *                                                    //  Ask her again.';
  */
-function willYouMarryMe(/* isPositiveAnswer */) {
-  throw new Error('Not implemented');
-}
+function willYouMarryMe(isPositiveAnswer) {
+  return new Promise((resolve, reject) => {
+    if (typeof isPositiveAnswer !== 'boolean') {
+      reject(new Error('Wrong parameter is passed! Ask her again.'));
+    }
+    let message;
+    if (isPositiveAnswer === true) {
+      message = 'Hooray!!! She said "Yes"!';
+    } else {
+      message = 'Oh no, she said "No".';
+    }
 
+    resolve(message);
+  });
+}
 
 /**
  * Return Promise object that should be resolved with array containing plain values.
@@ -48,8 +58,8 @@ function willYouMarryMe(/* isPositiveAnswer */) {
  *    })
  *
  */
-function processAllPromises(/* array */) {
-  throw new Error('Not implemented');
+function processAllPromises(array) {
+  return Promise.all(array);
 }
 
 /**
@@ -71,8 +81,8 @@ function processAllPromises(/* array */) {
  *    })
  *
  */
-function getFastestPromise(/* array */) {
-  throw new Error('Not implemented');
+function getFastestPromise(array) {
+  return Promise.race(array);
 }
 
 /**
@@ -92,8 +102,19 @@ function getFastestPromise(/* array */) {
  *    });
  *
  */
-function chainPromises(/* array, action */) {
-  throw new Error('Not implemented');
+/* eslint-disable no-param-reassign */
+/* eslint no-return-assign: 2,
+         no-extra-parens: 1,
+         brace-style: 2,
+         semi: 2
+*/
+async function chainPromises(array, action) {
+  const arr = [];
+  const result = await new Promise((resolve) => {
+    array.forEach((item, i) => item.then((res) => arr.push(res)).catch(() => (i += 1)));
+    resolve(arr);
+  });
+  return result.reduce(action);
 }
 
 module.exports = {
